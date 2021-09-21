5efe4363c028a70ce38611055f118cae104d7a89
vamseevk <vamseevk2001@gmail.cmo>
2021-09-21 14:04:47 +0530

    signin UI

---
+++ app/src/main/java/vamsee/application/studytube/Explore.kt
@@ -0,0 +1,11 @@
+package vamsee.application.studytube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Explore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explore)
+    }
+}
---
+++ app/src/main/java/vamsee/application/studytube/Wishlist.kt
@@ -0,0 +1,11 @@
+package vamsee.application.studytube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Wishlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wishlist)
+    }
+}
