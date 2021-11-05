a2a763fc1fab3dd2985452373e68e2ca8c0960fb
m3rcury02 <gunal2002@gmail.com>
2021-11-05 17:52:24 +0530

    Changed size of cards and added scaletype in Explore activity_explore.xml Created Bottom navigation View

--- app/src/main/java/vamsee/application/studytube/Dashboard.kt
+++ app/src/main/java/vamsee/application/studytube/Dashboard.kt
@@ -2,10 +2,18 @@ package vamsee.application.studytube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val bottNavView=findViewById<BottomNavigationView>(R.id.bottnav)
+        val navController =findNavController(R.id.fragment)
+        bottNavView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/vamsee/application/studytube/MainActivity.kt
+++ app/src/main/java/vamsee/application/studytube/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
