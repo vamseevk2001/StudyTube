41b6318355efed55cf1b716131f06b4e787158f8
vamseevk <vamseevk2001@gmail.cmo>
2021-12-01 12:00:43 +0530

    changed key

--- app/src/main/java/vamsee/application/studytube/API/SimpleApi.kt
+++ app/src/main/java/vamsee/application/studytube/API/SimpleApi.kt
@@ -7,14 +7,14 @@ import vamsee.application.studytube.Models.SearchItems
 import vamsee.application.studytube.Models.Video.VideoListResponse
 
 interface SimpleApi {
-    @GET("search?&maxResults=25&key=AIzaSyCod8lPe1sTf5y90E3ScvkTbZ_Ybl4xfs0")
+    @GET("search?&maxResults=25&key=AIzaSyD36dzhnPmdlveean9Z33K9_9us5pUIwHI")
     //@GET("&maxResults=25&key=AIzaSyD36dzhnPmdlveean9Z33K9_9us5pUIwHI")
     suspend fun search(@Query("q") name: String): retrofit2.Response<SearchItems>
 
-    @GET("videos?part=snippet%2CcontentDetails%2Cstatistics&key=AIzaSyCod8lPe1sTf5y90E3ScvkTbZ_Ybl4xfs0")
+    @GET("videos?part=snippet%2CcontentDetails%2Cstatistics&key=AIzaSyD36dzhnPmdlveean9Z33K9_9us5pUIwHI")
     suspend fun getVideoDetails(@Query("id") id: String): retrofit2.Response<VideoListResponse>
 
-    @GET("channels?part=snippet%2Cstatistics&key=AIzaSyCod8lPe1sTf5y90E3ScvkTbZ_Ybl4xfs0")
+    @GET("channels?part=snippet%2Cstatistics&key=AIzaSyD36dzhnPmdlveean9Z33K9_9us5pUIwHI")
     suspend fun getChannelDetails(@Query("id") id: String): retrofit2.Response<Channel>
 
 }
